b127ae913ede2aa57797b05c627b64e6f8a15e4b
Michael Doctor <michaelcdoctor@gmail.com>
2020-07-06 17:44:53 -0600

    Better way to save and grab secrets. Might need to fix when deploying.

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -1,7 +1,4 @@
 import os
-from environ import Env
-env = Env()
-env.read_env()
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -12,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', os.path.join(BASE_DIR, '.secrets'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
