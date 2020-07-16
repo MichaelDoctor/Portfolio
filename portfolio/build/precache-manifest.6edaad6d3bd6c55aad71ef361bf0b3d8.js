472c41dfa95d082432fa4a7934ad89a4679b4ed6
Michael Doctor <michaelcdoctor@gmail.com>
2020-07-16 10:46:38 -0600

    pusing build. Fix when pusing to origin

---
+++ portfolio/build/precache-manifest.6edaad6d3bd6c55aad71ef361bf0b3d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1d517e5330369ccb1fa0dc76ba4254",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3299ca87e8c53ffcb8d3",
+    "url": "./static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "./static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3299ca87e8c53ffcb8d3",
+    "url": "./static/js/main.bf8e6b4c.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
