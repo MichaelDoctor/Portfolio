e7d47116bf62847b9e4b4d494a772e6dc86ece84
Michael Doctor <michaelcdoctor@gmail.com>
2020-07-16 10:50:28 -0600

    Adding heroku to allowed hosts

--- project_backend/settings.py
+++ project_backend/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = ['localhost', 'michael-doctor.me', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', 'michael-doctor.me',
+                 'michael-doctor.herokuapp.com']
 
 
 # Application definition
