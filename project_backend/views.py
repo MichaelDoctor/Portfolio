e111d5d842f51fbbc44917fee73323d8b3c572ad
Michael Doctor <michaelcdoctor@gmail.com>
2020-08-02 10:16:29 -0600

    Added else

--- project_backend/views.py
+++ project_backend/views.py
@@ -10,3 +10,5 @@ class CurrentUser(APIView):
         user = request.user
         if user.is_authenticated:
             return Response({'pk': user.id, 'username': user.username, 'first_name': user.first_name, 'last_name': user.last_name})
+        else:
+            return Response(user)
