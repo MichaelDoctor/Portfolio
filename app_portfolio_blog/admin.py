d4247c8c8d0e6a0bc14fc4443ad49fa8338023fc
Michael Doctor <michaelcdoctor@gmail.com>
2020-08-06 14:07:07 -0600

    Add to admin

--- app_portfolio_blog/admin.py
+++ app_portfolio_blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import BlogPost, Comment
+from .serializers import BlogPostSerializer, CommentSerializer
 
-# Register your models here.
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'img', 'author', 'date', 'content')
+    list_display_links = ('id', 'title', 'date')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'blog', 'parent', 'author', 'content', 'date')
+    list_display_links = ('id', 'author', 'date')
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(Comment, CommentAdmin)
