3343072e3c0b3c29bcb2ec52369d814350377596
Michael Doctor <michaelcdoctor@gmail.com>
2020-08-08 16:43:24 -0600

    Added get user endpoint

--- project_backend/urls.py
+++ project_backend/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import CurrentUser
+from .views import CurrentUser, GetUsername
 
 urlpatterns = [
     path('api/skills/', include('app_portfolio_skills.urls')),
@@ -14,6 +14,7 @@ urlpatterns = [
     path('allauth/', include('allauth.urls')),
     path('auth/register/', include('dj_rest_auth.registration.urls')),
     path('auth/current/', CurrentUser.as_view()),
+    path('auth/user/<pk>', GetUsername.as_view()),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # creates urls for media
 
--- project_backend/views.py
+++ project_backend/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from django.contrib.auth.models import User
 
 
 class CurrentUser(APIView):
@@ -19,3 +20,20 @@ class CurrentUser(APIView):
             return Response(content)
         else:
             return Response({'pk': 'None'})
+
+
+class GetUsername(APIView):
+    permission_classes = (permissions.AllowAny,)
+    lookup_field = 'pk'
+
+    def get(self, request, pk):
+        try:
+            data = User.objects.filter(id=pk).values()[0]
+            content = {
+                'pk': data[pk],
+                'username': data['username']
+            }
+            return Response(content)
+        except:
+
+            return Response({'pk': pk, 'username': 'Anon'})
