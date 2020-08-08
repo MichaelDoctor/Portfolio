225306f56afa1116661354cb4df00e9e7daceb52
Michael Doctor <michaelcdoctor@gmail.com>
2020-08-08 16:00:16 -0600

    Finished actions

--- frontend/src/redux/actions/blog.js
+++ frontend/src/redux/actions/blog.js
@@ -9,15 +9,26 @@ const baseUrl = 'https://michael-doctor.me';
 // List of posts
 export const getPostList = () => (dispatch) => {
 	axios.get(`${baseUrl}/api/blogs/`).then((res) => {
-		console.log(res);
+		dispatch({
+			type    : GET_POSTS,
+			payload : res.data
+		});
 	});
 };
 
 // Detail post view
 export const getPost = (slug) => (dispatch) => {
-	axios.get(`${baseUrl}/api/blogs/blog/${slug}`).then((res) => {
-		console.log(res);
-	});
+	axios
+		.get(`${baseUrl}/api/blogs/blog/${slug}`)
+		.then((res) => {
+			dispatch({
+				type    : GET_POST,
+				payload : res.data
+			});
+		})
+		.catch((err) => {
+			dispatch(setAlert('Post does not exist', 'danger'));
+		});
 };
 
 //Create post
@@ -33,15 +44,20 @@ export const createPost = ({ formData, csrfmiddlewaretoken }) => (dispatch) => {
 		.post(`${baseUrl}/api/blogs/create/post/`, formData, config)
 		.then((res) => {
 			dispatch(setAlert('Post Successfully Created', 'success'));
+			dispatch({
+				type    : CREATE_POST,
+				payload : res.data
+			});
 		})
 		.catch((err) => {
+			dispatch(setAlert('Error Creating Post', 'danger'));
 			dispatch(setErrors(err.response.data));
 			dispatch(errorMessage(err.response.data, err.response.status));
 		});
 };
 
 //Create comment
-export const createComment = ({ formData, csrfmiddlewaretoken }) => (dispatch) => {
+export const createComment = ({ formData, csrfmiddlewaretoken, slug }) => (dispatch) => {
 	const config = {
 		headers : {
 			'Content-Type' : 'multipart/form-date',
@@ -52,9 +68,11 @@ export const createComment = ({ formData, csrfmiddlewaretoken }) => (dispatch) =
 	axios
 		.post(`${baseUrl}/api/blogs/create/comment/`, formData, config)
 		.then((res) => {
-			dispatch(setAlert('Comment Successfully Created', 'success'));
+			console.log(res);
+			getPostList(slug);
 		})
 		.catch((err) => {
+			dispatch(setAlert('Error creating comment', 'danger'));
 			dispatch(setErrors(err.response.data));
 			dispatch(errorMessage(err.response.data, err.response.status));
 		});
