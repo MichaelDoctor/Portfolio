a932aaa3289cd1e457fefe7528ee6f43c874da38
Michael Doctor <michaelcdoctor@gmail.com>
2021-01-21 22:46:35 -0700

    Changed about description

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -12,21 +12,18 @@ const About = () => {
           </div>
           <div className="hr"></div>
           <div className="about-sub">
-            CALGARY BASED SOFTWARE DEVELOPMENT STUDENT AND AN ASPIRING
-            FULL-STACK WEB DEVELOPER
+            CALGARY BASED SOFTWARE DEVELOPMENT STUDENT. ASPIRING
+            FULL-STACK WEB AND iOS DEVELOPER.
           </div>
         </div>
         <div className="section-desc slide-left">
           <div className="about-divider"></div>
           <div className="about-desc">
             Currently, I&#x27;m a software development student at Southern
-            Alberta Institute of Technology (SAIT). I&#x27;m passionate in
-            full-stack web development. My preferred frontend and backend
-            frameworks are React and Django, respectively. The programming
-            languages I excel in are Python and JavaScript. I enjoy creating
-            responsive and dynamic web apps by integrating bootstrap with React.
-            Along with web development, I&#x27;m interested in learning about
-            Artificial Intelligence using Python.
+            Alberta Institute of Technology (SAIT). I&#x27;m passionate about
+            full-stack web development and iOS development. My preferred
+            frameworks are React, Express, and UIKit. The programming
+            languages that I use most are Javascript and Swift.
           </div>
           <div className="about-divider end"></div>
         </div>
